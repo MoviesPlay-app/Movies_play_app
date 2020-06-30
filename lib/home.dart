285d833a9334ae4b69e896775ee43ee52816870c
MoviesPlay-app <moviesplays49@gmail.com>
2020-06-30 23:40:14 +0530

    home screen UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:slimy_card/slimy_card.dart';
+//import 'package:awesome_card/awesome_card.dart';
 // To be done 
 class MidScreen extends StatefulWidget {
   @override
@@ -24,24 +27,329 @@ class _MidScreenState extends State<MidScreen> {
   }
 }
 
+
+
 class UserDisplayHome extends StatefulWidget {
   @override
-  _UserDisplayHomeState createState() => _UserDisplayHomeState();
+  _UserDisplayHome createState() => _UserDisplayHome();
 }
 
-class _UserDisplayHomeState extends State<UserDisplayHome> {
-  
-  
+class _UserDisplayHome extends State<UserDisplayHome> with SingleTickerProviderStateMixin {
+  bool isCollapsed = true;
+  double screenWidth, screenHeight;
+  final Color backgroundColor = Color(0xFFFAFAFA);
+  final Duration duration = const Duration(milliseconds: 300);
+  AnimationController _controller;
+  Animation<double> _scaleAnimation;
+  Animation<double> _menuScaleAnimation;
+  Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: duration);
+    _scaleAnimation = Tween<double>(begin: 1, end: 0.8).animate(_controller);
+    _menuScaleAnimation = Tween<double>(begin: 0.5, end: 1).animate(_controller);
+    _slideAnimation = Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0)).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-   
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: <Widget>[
+          menu(context),
+          dashboard(context),
+        ],
       ),
-      body : Container(
-        
-      )
+    );
+  }
+
+  Widget menu(context) {
+    return SlideTransition(
+      position: _slideAnimation,
+      child: ScaleTransition(
+        scale: _menuScaleAnimation,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Dashboard", style: TextStyle(color: Colors.black, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Messages", style: TextStyle(color: Colors.black, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Utility Bills", style: TextStyle(color: Colors.black, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Funds Transfer", style: TextStyle(color: Colors.black, fontSize: 22)),
+                SizedBox(height: 10),
+                Text("Branches", style: TextStyle(color: Colors.black, fontSize: 22)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget dashboard(context) {
+    return AnimatedPositioned(
+      duration: duration,
+      top: 0,
+      bottom: 0,
+      left: isCollapsed ? 0 : 0.6 * screenWidth,
+      right: isCollapsed ? 0 : -0.2 * screenWidth,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Material(
+          animationDuration: duration,
+          borderRadius: BorderRadius.all(Radius.circular(40)),
+          elevation: 8,
+          color: backgroundColor,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      InkWell(
+                        child: Icon(Icons.menu, color: Colors.black),
+                        onTap: () {
+                          setState(() {
+                            if (isCollapsed)
+                              _controller.forward();
+                            else
+                              _controller.reverse();
+
+                            isCollapsed = !isCollapsed;
+                          });
+                        },
+                      ),
+                      Text("Personalised List", style: TextStyle(fontSize: 24, color: Colors.black)),
+                      Icon(Icons.settings, color: Colors.black),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction: 0.8),
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      pageSnapping: true,
+                      children: <Widget>[
+                        /*Container(
+                          child : SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          physics: BouncingScrollPhysics(),
+                          child: SlimyCard(
+                            color: Colors.red,
+                            width:200,
+                            topCardHeight:150,
+                            bottomCardHeight: 100,
+                            borderRadius: 15,
+                            slimeEnabled:true,
+                          ),
+                          ),
+                         
+                        ),*/
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 70),
+                  Text("Cinematography", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  SizedBox(height:10),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction:0.6),
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                    ],)
+                  ),
+                  SizedBox(height:40),
+                  Text("Genre", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  SizedBox(height:10),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction:0.6),
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                    ],)
+                  ),
+
+                  SizedBox(height:40),
+                  Text("Cast", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  SizedBox(height:10),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction:0.6),
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                    ],)
+                  ),
+                  SizedBox(height:40),
+                  Text("Language", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  SizedBox(height:10),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction:0.6),
+                      scrollDirection: Axis.horizontal,
+                      physics: BouncingScrollPhysics(),
+                      children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                    ],)
+                  ),
+                  SizedBox(height:40),
+                  Text("Plot", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  SizedBox(height:10),
+                  Container(
+                    height: 200,
+                    child: PageView(
+                      controller: PageController(viewportFraction:0.6),
+                      scrollDirection: Axis.horizontal,
+                      physics:BouncingScrollPhysics(),
+                      children: <Widget>[
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.redAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.blueAccent,
+                          width: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8),
+                          color: Colors.greenAccent,
+                          width: 100,
+                        ),
+                    ],)
+                  )
+                  /*SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  physics:BouncingScrollPhysics(),
+                  child:ListView.separated(
+                    shrinkWrap: true,
+                      physics:BouncingScrollPhysics(),
+                      itemBuilder: (context, position) {
+                    return ListTile(
+                      title: Text(position.toString()),
+                      subtitle: Text("Apple"),
+                      trailing: Text("-2900"),
+                    );
+                  }, separatorBuilder: (context, position) {
+                    return Divider(height: 16);
+                  }, itemCount: 10)
+                  ),*/
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
